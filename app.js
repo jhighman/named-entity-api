d6bd5bbe8b4d94144e1c1e603314fa907d00c9ac
jhighman <jhighman@gmail.com>
2024-01-17 14:09:53 -0500

    make it better

--- app.js
+++ app.js
@@ -48,9 +48,4 @@ export default async function (fastify, opts) {
     options: Object.assign({}, opts)
   })
 
-  fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
-    if (err) throw err
-    fastify.log.info(`server listening on ${address}`)
-  })
-
 }
